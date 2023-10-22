76fff851f7d97d360cb51daba6ad94883435c1f6
Andrea Fernández <57370524+iandreafh@users.noreply.github.com>
2023-10-22 14:06:51 +0200

    Primer commit de prueba en el repositorio para ir subiendo y actualizando los cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // ... otras rutas,
+  { path: 'welcomeMi', component: WelcomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
